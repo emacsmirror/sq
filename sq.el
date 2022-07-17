bb0d1756bb9c62bb999fa21e15949e28650c663e
Justus Winter <justus@sequoia-pgp.org>
2022-07-17 22:37:35 +0200

    Release 0.1.2.

--- sq.el
+++ sq.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021 Justus Winter
 
 ;; Author: Justus Winter <justus@sequoia-pgp.org>
-;; Version: 0.1.1
+;; Version: 0.1.2
 ;; Created: 23 Jun 2021
 ;; Keywords: tools data mail
 ;; URL: https://gitlab.com/sequoia-pgp/sqel
